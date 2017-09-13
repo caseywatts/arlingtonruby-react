decf92185f14befad0fe1df8b48e6d3364940bb3
Jon Keam <jpkeam@gmail.com>
2017-09-13 14:06:33 -0400

    minor refactor to add proptypes and some basic error handling

--- src/MeetupCard.js
+++ src/MeetupCard.js
@@ -1,31 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MeetupCard extends Component {
   render() {
-    if (this.props.meetupEvent) {
-      return (
-        <div className="panel panel-default">
-          <div className="panel-heading">
-            <a href={this.props.meetupEvent.event_url} target="_blank">{this.props.meetupEvent.name}</a>
+    const { name, event_url, description, formattedDate, venueLink, venue, yes_rsvp_count } = this.props.meetupEvent;
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <a href={event_url} target="_blank">{name}</a>
+        </div>
+        <div className="panel-body">
+          <div className="col-sm-8">
+            <p dangerouslySetInnerHTML={ { __html: description }}></p>
           </div>
-          <div className="panel-body">
-            <div className="col-sm-8">
-              <p dangerouslySetInnerHTML={ { __html: this.props.meetupEvent.description }}></p>
-            </div>
-            <div className="col-sm-4">
-              <b>WHEN:</b> {this.props.meetupEvent.formattedDate}
-              <br /><b>WHERE:</b> <a href={this.props.meetupEvent.venueLink} target="_blank">{this.props.meetupEvent.venue.name}</a>
-              <br /><b>WHO:</b> {this.props.meetupEvent.yes_rsvp_count} already going
-            </div>
+          <div className="col-sm-4">
+            <b>WHEN:</b> {formattedDate}
+            <br /><b>WHERE:</b> <a href={venueLink} target="_blank">{venue.name}</a>
+            <br /><b>WHO:</b> {yes_rsvp_count} already going
           </div>
         </div>
-      );
-    } else {
-      return (
-        <div></div>
-      )
-    }
+      </div>
+    );
   }
 }
+
+MeetupCard.propTypes = {
+  meetupEvent: PropTypes.object.isRequired
+};
 
 export default MeetupCard;
--- src/MeetupCardList.js
+++ src/MeetupCardList.js
@@ -1,38 +1,52 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MeetupCard from './MeetupCard';
 import EventPresenter from './EventPresenter'
 import fetchMeetups from './fetchMeetups'
 
 class MeetupCardList extends Component {
-  static defaultProps = { numOfCardsToShow: 5 }
-  constructor() {
-    super();
-    this.state = { meetupEvents: [] };
+  constructor(props) {
+    super(props);
+    this.state = {
+      meetupEvents: [],
+      errorText: null
+    };
   }
+
   componentDidMount() {
     fetchMeetups().then((responseData) => {
-      const someEvents = responseData.results.slice(0,this.props.numOfCardsToShow);
-      const decoratedMeetupEvents = someEvents.map((rawEventData) => {
-        return EventPresenter(rawEventData);
-      })
+      const someEvents = (responseData.results || []).slice(0, this.props.numOfCardsToShow);
+      const decoratedMeetupEvents = someEvents.map((rawEventData) => EventPresenter(rawEventData));
       this.setState({meetupEvents: decoratedMeetupEvents});
-    })
+    }).catch((e) => {
+      console.error(`Unable to fetch data.`, e);
+      this.setState({errorText: `Encountered error while trying to fetch meetup data.  Please wait and reload the page.`})
+    });
   }
+
   render() {
-    if (this.state.meetupEvents) {
-      return (
-        <div>
-          {this.state.meetupEvents.map((meetupEvent) =>
-            <MeetupCard meetupEvent={meetupEvent} key={meetupEvent.id} />
-          )}
-        </div>
-      );
-    } else {
-      return (
-        <div></div>
-      )
-    }
+    const { errorText, meetupEvents } = this.state;
+    return (
+      <div>
+        {
+          !errorText && (meetupEvents || []).map((meetupEvent) =>
+            <MeetupCard meetupEvent={meetupEvent} key={meetupEvent.id} />)
+        }
+
+        {
+          errorText && <p className="text-danger">{errorText}</p>
+        }
+      </div>
+    );
   }
 }
+
+MeetupCardList.propTypes = {
+  numOfCardsToShow: PropTypes.number
+};
+
+MeetupCardList.defaultProps = {
+  numOfCardsToShow: 5
+};
 
 export default MeetupCardList;
